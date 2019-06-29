f9b1e8a4d3184b8cb3bcebce6c1ebb976766d78c
Chenxi-yi <52072896+Chenxi-yi@users.noreply.github.com>
2019-06-29 19:47:28 +0100

    Update 包含min函数的栈.py

--- 剑指offer/包含min函数的栈.py
+++ 剑指offer/包含min函数的栈.py
@@ -41,3 +41,30 @@ class Solution:
     def min(self):
         # write code here
         return self.minstack[-1]
+ # 编译没通过orz
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.assist = []
+         
+    def push(self, node):
+        min = self.min()
+        if not min or node < min:
+            self.assist.append(node)
+        else:
+            self.assist.append(min)
+        self.stack.append(node)
+         
+    def pop(self):
+        if self.stack:
+            self.assist.pop()
+            return self.stack.pop()
+    def top(self):
+        # write code here
+        if self.stack:
+            return self.stack[-1]
+         
+    def min(self):
+        # write code here
+        if self.assist:
+            return self.assist[-1]

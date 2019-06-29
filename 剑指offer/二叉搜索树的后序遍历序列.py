d03fa163a0a58377ca2c0d265ef96517a4ed9ff9
Chenxi-yi <52072896+Chenxi-yi@users.noreply.github.com>
2019-06-29 21:22:28 +0100

    Update 二叉搜索树的后序遍历序列.py

--- 剑指offer/二叉搜索树的后序遍历序列.py
+++ 剑指offer/二叉搜索树的后序遍历序列.py
@@ -50,3 +50,55 @@ class Solution:
         return left and right
 
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    class Solution:
+    def VerifySquenceOfBST(self, sequence):
+        # write code here
+        '''
+二叉搜索树即是二叉排序树，
+1. 后序遍历序列的最后一个元素为二叉树的根节点；
+2. 二叉搜索树左子树上所有的结点均小于根结点、右子树所有的结点均大于根结点。
+算法步骤如下：
+1. 找到根结点；
+2. 遍历序列，找到第一个大于等于根结点的元素i，则i左侧为左子树、i右侧为右子树；
+3. 我们已经知道i左侧所有元素均小于根结点，那么再依次遍历右侧，看是否所有元素均大于根结点；若出现小于根结点的元素，则直接返回false；若右侧全都大于根结点，则：
+4. 分别递归判断左/右子序列是否为后序序列；
+'''
+
+        if sequence==None or len(sequence)==0:
+            return False
+        length=len(sequence)
+        root=sequence[length-1]
+        # 后序遍历中，最后一位肯定是根节点，完整排列为 左 右 根。 
+        # 二叉搜索树左子树值都比root小，右子树值都比root大。 对应 比root小 root 比root大
+        
+        # 在二叉搜索树中 左子树节点小于根节点
+        for i in range(length):
+            if sequence[i]>root:
+                break
+        # 二叉搜索树中右子树的节点都大于根节点
+        for j  in range(i,length):
+            if sequence[j]<root:
+                return False
+        # 判断左子树是否为二叉树
+        left=True
+        if  i>0:
+            left=self.VerifySquenceOfBST(sequence[0:i])
+        # 判断 右子树是否为二叉树
+        right=True
+        if i<length-1:
+            right=self.VerifySquenceOfBST(sequence[i:-1])
+        return left and right
+
